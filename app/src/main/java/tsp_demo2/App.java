130f62ecb8856638c270e54d9c69aaf56a7c13e4
Kurt Wilson <kurt4wilson@gmail.com>
2022-03-24 16:35:06 -0400

    adjust ant parameters

--- app/src/main/java/tsp_demo2/App.java
+++ app/src/main/java/tsp_demo2/App.java
@@ -12,15 +12,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         // print current resource path
         System.out.println(System.getProperty("user.dir"));
-        Graph g = Graph.from_tsplib("solved_graphs/eil76.tsp");
-        g.optimal_tour = Graph.read_tour("solved_graphs/eil76.opt.tour");
+        Graph g = Graph.from_tsplib("solved_graphs/tsp225.tsp");
+        g.optimal_tour = Graph.read_tour("solved_graphs/tsp225.opt.tour");
         System.out.printf("Optimal tour length: %f\n", g.get_tour_length(g.optimal_tour));
         // System.out.println(g.optimal_tour);
         List<Integer> tour = GreedyNearest.find(g);
         System.out.printf("Greedy tour length: %f\n", g.get_tour_length(tour));
         List<Integer> christofides = Christofides.find(g);
         System.out.printf("Christofides tour length: %f\n", g.get_tour_length(christofides));
-        List<Integer> ant_col_tour = SerialAntColony.find(g, 100, 100);
+        // turns we don't need a lot of ants or iterations to get a good result
+        List<Integer> ant_col_tour = SerialAntColony.find(g, 10, 10);
         System.out.printf("Ant colony tour length: %f\n", g.get_tour_length(ant_col_tour));
 
     }
